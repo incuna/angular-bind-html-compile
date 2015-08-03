8186b97ddabc2dfbf78bf405f64b4dbd58faa689
Filipe Silva <filipematossilva@gmail.com>
2015-08-03 23:07:48 +0100

    Remove isolate scope keeping bindHtmlScope

--- angular-bind-html-compile.js
+++ angular-bind-html-compile.js
@@ -6,18 +6,16 @@
     module.directive('bindHtmlCompile', ['$compile', function ($compile) {
         return {
             restrict: 'A',
-            scope: {
-                bindHtmlCompile: '=',
-                bindHtmlScope: '=?'
-            },
-            link: function (scope, element, attrs) {
-                scope.$watch('bindHtmlCompile', function (value) {
+            link: function(scope, element, attrs) {
+                scope.$watch(function() {
+                    return scope.$eval(attrs.bindHtmlCompile);
+                }, function(value) {
                     // Incase value is a TrustedValueHolderType, sometimes it
                     // needs to be explicitly called into a string in order to
                     // get the HTML string.
                     element.html(value && value.toString());
                     // If scope is provided use it, otherwise use parent scope
-                    var compileScope = scope.bindHtmlScope || scope.$parent;
+                    var compileScope = scope.$eval(attrs.bindHtmlScope) || scope.$parent;
                     $compile(element.contents())(compileScope);
                 });
             }
