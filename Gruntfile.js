782d85d484295553695ba84b46dfe035874ff0a0
Graham Gilchrist <mail@grahamgilchrist.com>
2015-08-06 14:17:19 +0100

    Add gruntfile with linting tasks

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var _ = require('lodash');
+var fs = require('fs');
+
+module.exports = function (grunt) {
+    require('time-grunt')(grunt);
+
+    grunt.initConfig({
+        // Configurable paths
+        config: {
+            lintFiles: [
+                '**/*.js'
+            ]
+        },
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc'
+            },
+            all: [
+                '<%= config.lintFiles %>'
+            ]
+        },
+        jscs: {
+            options: {
+                config: '.jscsrc'
+            },
+            src: '<%= config.lintFiles %>'
+        }
+    });
+
+    grunt.registerTask('lint', [
+        'jshint',
+        'jscs'
+    ]);
+
+    grunt.registerTask('test', [
+        'lint'
+    ]);
+};
