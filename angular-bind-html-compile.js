1c638987cdf79f5c88c27c30c30ff6701d24d140
Jim LoVerde <loverde@nvisia.com>
2016-04-07 11:46:55 -0500

    fixing linter issues

--- angular-bind-html-compile.js
+++ angular-bind-html-compile.js
@@ -4,37 +4,41 @@
     var module = angular.module('angular-bind-html-compile', []);
 
     module.directive('bindHtmlCompile', ['$compile', function ($compile) {
-      return {
-          restrict: 'A',
-          scope: {
-            beforeCompile:'&bindHtmlBeforeCompile',
-            afterCompile: '&bindHtmlAfterCompile'
-          },
-          link: function (scope, element, attrs) {
-              scope.$parent.$watch(function () {
-                  return scope.$parent.$eval(attrs.bindHtmlCompile);
-              }, function (value) {
-                  // In case value is a TrustedValueHolderType, sometimes it
-                  // needs to be explicitly called into a string in order to
-                  // get the HTML string.
-                  element.html(value && value.toString());
+        return {
+            restrict: 'A',
+            scope: {
+                beforeCompile: '&bindHtmlBeforeCompile',
+                afterCompile: '&bindHtmlAfterCompile'
+            },
+            link: function (scope, element, attrs) {
+                scope.$parent.$watch(function () {
+                    return scope.$parent.$eval(attrs.bindHtmlCompile);
+                }, function (value) {
+                    // In case value is a TrustedValueHolderType, sometimes it
+                    // needs to be explicitly called into a string in order to
+                    // get the HTML string.
+                    element.html(value && value.toString());
 
-                  // beforeCompile hook
-                  var beforeCompile = scope.beforeCompile();
-                  beforeCompile && beforeCompile(element)
+                    // beforeCompile hook
+                    var beforeCompile = scope.beforeCompile();
+                    if (angular.isFunction(beforeCompile)) {
+                        beforeCompile(element);
+                    }
 
-                  // If scope is provided use it, otherwise use parent scope
-                  var compileScope = scope.$parent;
-                  if (attrs.bindHtmlScope) {
-                      compileScope = scope.$parent.$eval(attrs.bindHtmlScope);
-                  }
-                  $compile(element.contents())(compileScope);
+                    // If scope is provided use it, otherwise use parent scope
+                    var compileScope = scope.$parent;
+                    if (attrs.bindHtmlScope) {
+                        compileScope = scope.$parent.$eval(attrs.bindHtmlScope);
+                    }
+                    $compile(element.contents())(compileScope);
 
-                  // After compile hook
-                  var afterCompile = scope.afterCompile();
-                  afterCompile && afterCompile(element)
-              });
-          }
+                    // After compile hook
+                    var afterCompile = scope.afterCompile();
+                    if (angular.isFunction(afterCompile)) {
+                        afterCompile(element);
+                    }
+                });
+            }
       };
     }]);
 }(window.angular));
