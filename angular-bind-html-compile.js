4d792fa1d46fdb5274eb7eed20a515e1fd4623c5
Ibrahim Hd <hammadibrahim@hotmail.com>
2016-12-05 10:39:42 +0200

    Support for template URL
    attribute: template-url="'myTemplateUrl'"

--- angular-bind-html-compile.js
+++ angular-bind-html-compile.js
@@ -39,7 +39,7 @@
                                 if (scope.$$destroyed) {return;}
                             });
                         }
-                });
+                  });
             }
         };
     }]);
